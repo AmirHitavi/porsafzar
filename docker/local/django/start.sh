601c0859666f2771c2e01db1b7799f2a0bbcf8c0
Amir Hitavi <amirhitavi3@gmail.com>
2025-10-22 11:17:32 +0330

    docker[bugfix]: resolve issues for make migrattions and migrate in container.

--- docker/local/django/start.sh
+++ docker/local/django/start.sh
@@ -4,9 +4,9 @@ set -o errexit
 set -o nounset
 
 
-python manage.py migrate django_celery_beat
 
 python manage.py makemigrations --no-input
+python manage.py migrate django_celery_beat
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
